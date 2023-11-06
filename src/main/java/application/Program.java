ec35c0988a10f28561af00f2a0c5efcc8169ced3
NicollasMelo <nicollasmelo2001@gmail.com>
2023-11-06 13:43:09 -0300

    get seller

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,15 +1,17 @@
 package application;
 
-import java.util.Date;
-import model.entities.Department;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "Books");
-        System.out.print(department);
 
-        Seller seller = new Seller(1, "Nicollas","nicollas@gmail.com", new Date(), 3000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
         System.out.print(seller);
     }
 }
