9e54a7da17d9ccbcc7e7346090234bc0e3d04bee
NicollasMelo <nicollasmelo2001@gmail.com>
2023-11-06 13:11:31 -0300

    seller

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.print(department);
+
+        Seller seller = new Seller(1, "Nicollas","nicollas@gmail.com", new Date(), 3000.0, department);
+        System.out.print(seller);
     }
 }
